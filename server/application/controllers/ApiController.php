c2fafbac986bb5e9234a4c9e8402d1518b6598b1
Charles Zhou <fidy.watcher@gmail.com>
2013-10-07 15:13:17 +0800

    adding get-next-question api
    Change-Id: I2e256e4e8c0ec8feb4c7e231fcf4d6ea3e8e1274

---
+++ server/application/controllers/ApiController.php
@@ -0,0 +1,59 @@
+<?php
+
+class ApiController extends My_Controller_Action
+{
+
+    public function init()
+    {
+    	parent::init();
+    }
+
+
+    public function indexAction()
+    {
+    	
+
+    }
+    
+    public function getNextQustionAction(){
+    	Zend_Layout::getMvcInstance()->disableLayout();
+    	$questionTable = new Model_DbTable_Questions();
+    	$answerTable = new Model_DbTable_Answers();
+    	$question = $questionTable->fetchRow("id = 2");
+    	$answers = $answerTable->fetchAll("question_id = 2");
+    	$data["question"] = $question->term . rand(10, 10000);
+    	$data["question_id"] = $question->id;
+    	$data["audio"] = "http://dalianhua.uiu.cc" . $question->audio;
+    	foreach ($answers as $answer){
+    		$theAnswer["content"] = $answer->option_content;
+    		$theAnswer["correct"] = $answer->is_correct;
+    		$data["answers"][] = $theAnswer;
+    	}
+    	
+    	echo Zend_Json::encode($data);
+    	
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
